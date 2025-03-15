39da1966b68f52702e877ff81e9b96f93f593d5b
SeetaRam <seetaramanjaneyulu172002@gmail.com>
2025-03-15 23:46:15 +0530

    Connected the application with the mongodb database

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 const express = require("express");
+const connectToDatabase = require("./config/database");
 
 const app = express();
 
@@ -8,6 +9,13 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ msg: "Default route" });
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+connectToDatabase()
+  .then(() => {
+    console.log("Database connected successFully...");
+    app.listen(3000, () => {
+      console.log("Listening on port 3000");
+    });
+  })
+  .catch(() => {
+    console.error("Database connection failed!!!");
+  });
